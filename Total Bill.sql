868a591f18b6989014a7cb21be87d64c66588f40
ANNE WANGARI NJOROGE <77064798+Anne-Wangary@users.noreply.github.com>
2023-05-11 18:57:32 +0300

    Update Total Bill.sql

--- Total Bill.sql
+++ Total Bill.sql
@@ -3,4 +3,11 @@ kasarani.LastName,kasarani.Contact,kasarani.AmountPayable,
 KasaraniBills.KPLCBill,KasaraniBills.WaterBill,(WaterBill+KPLCBill+AmountPayable)
 AS TotalBill
 FROM KasaraniBills
-LEFT JOIN Kasarani ON kasaraniBills.HouseNumber=Kasarani.HouseNumber
+LEFT JOIN Kasarani ON kasaraniBills.HouseNumber=Kasarani.HouseNumber;
+
+SELECT kayole.HouseNumber,kayole.FirstName,
+kayole.LastName,kayole.Contact,kayole.AmountPayable,
+kayoleBills.KPLCBill,kayoleBills.WaterBill,(WaterBill+KPLCBill+AmountPayable)
+AS TotalBill
+FROM kayoleBills
+LEFT JOIN Kasarani ON kayoleiBills.HouseNumber=kayole.HouseNumber;
